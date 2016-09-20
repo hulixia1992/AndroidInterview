6c74b679a91f5a6b0741f3bf909b64552ab2dbf3
hulixia1992 <hulixia1992@gmail.com>
2016-09-20 14:07:55 +0800

    第一次提交

---
+++ AndroidInterview/app/src/androidTest/java/com/hulixia1992/interview/androidinterview/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.hulixia1992.interview.androidinterview;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
